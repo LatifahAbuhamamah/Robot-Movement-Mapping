3fa042c2ba8e2311857a922f209e54ae5c841831
LATIFAH ABUHAMAMAH <139233344+LatifahAbuhamamah@users.noreply.github.com>
2023-08-18 09:57:38 +0300

    Add files via upload

---
+++ movements.php
@@ -0,0 +1,28 @@
+<?php
+// Connect to your database (replace with your actual database connection code)
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "RobotMovements";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$data = json_decode(file_get_contents("php://input"), true);
+$direction = $data['direction'];
+
+$sql = "INSERT INTO robot_movements (direction) VALUES ('$direction')";
+
+if ($conn->query($sql) === true) {
+    $response = ['success' => true];
+} else {
+    $response = ['success' => false, 'error' => $conn->error];
+}
+
+$conn->close();
+
+header('Content-Type: application/json');
+echo json_encode($response);
+?>
